3fafd5364a4e0e3b8b8a3754c3003072b330dcbf
fatmaidris <fatma18650@gmail.com>
2023-12-05 12:10:56 -0800

    done

--- _printf.c
+++ _printf.c
@@ -7,34 +7,43 @@
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int l = 0, len = 0, c;
-	format_t w[] = {{"%c", print_char}, {"%s", printf_string},
-		{"%%", print_37}, {"%d", print_dec}, {"%S", print_str},
-		{"%i", print_int}, {"%b", print_binary}, {"%x", print_x},
-		{"%u", print_unsigned}, {"%o", print_octal}, {"%X", print_X},
-		{"%p", print_pointers}, {"%r", print_rev}, {"%R", print_rot},};
+	int l = 0;
+	char x, *st;
 
 	va_start(args, format);
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-		return (-1);
-Here:
-	while (format[l] == '\0')
+	while (*format)
 	{
-		c = 13;
-		while (c >= 0)
+		if (*format == '%')
 		{
-			if (w[c].id[0] == format[l] && w[c].id[1] == format[l + 1])
+			format++;
+			if (*format == '%')
 			{
-				len = len + w[c].w(args);
-				l = l + 2;
-				goto Here;
+				_putchar('%');
+				l++;
 			}
-			c--;
+			else if (*format == 'c')
+			{
+				x = va_arg(args, int);
+				_putchar(x);
+				l++;
+			}
+			else if (*format == 's')
+			{
+				st = va_arg(args, char *);
+				while (*st)
+				{
+					_putchar(*st);
+					st++;
+					l++;
+			}}}
+
+		else
+		{
+			_putchar(*format);
+			l++;
 		}
-		_putchar(format[l]);
-		l++;
-		len++;
+		format++;
 	}
 	va_end(args);
-	return (len);
+	return (l);
 }
